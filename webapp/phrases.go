8ddaf99533588039a5b733e592492c8e5db4617d
Josh Campbell <joshcb@amazon.com>
2018-09-18 17:08:46 -0700

    adds new phrases for more variety :fire:

--- webapp/phrases.go
+++ webapp/phrases.go
@@ -6,6 +6,8 @@ func phrases() []string {
 		"Needs more Bias for Action",
 		"Doesn't sound very Customer Obsessed",
 		"This is a Disagree and Commit moment for me",
+		"I'm going to have to push back and Insist on the Highest Standards",
+		"Getting beer for the overworked dev team isn't very Frugal",
 	}
 
 	return phrases
